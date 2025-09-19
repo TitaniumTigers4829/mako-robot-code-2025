22533509d2c6cf1d69655277f12d55692431901a
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2025-09-19 16:28:45 -0400

    fsszu

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "robot-code-2025-5";
+  public static final String MAVEN_NAME = "mako-robot-code-2025-3";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 247;
-  public static final String GIT_SHA = "6da958df57e66087c232807d8d0e3bf36ae3f328";
-  public static final String GIT_DATE = "2025-05-05 14:47:57 EDT";
+  public static final int GIT_REVISION = 248;
+  public static final String GIT_SHA = "adda8ad9b8d572b31c51860b76768c3130103225";
+  public static final String GIT_DATE = "2025-07-23 16:16:12 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-07-23 16:13:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1753301595625L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-09-17 17:49:36 EDT";
+  public static final long BUILD_UNIX_TIME = 1758145776876L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
