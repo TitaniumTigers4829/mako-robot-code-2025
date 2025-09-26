e65d05fb4d05d2eb656f72496c65347aa5cfcf7d
aridavidson001 <aridavidson6@gmail.com>
2025-09-26 12:42:33 -0400

    push to main trusttt

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "mako-robot-code-2025-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 251;
-  public static final String GIT_SHA = "2b7449b408ad8dda4b54205510fe13a0d728cc35";
-  public static final String GIT_DATE = "2025-09-24 18:06:22 EDT";
+  public static final int GIT_REVISION = 252;
+  public static final String GIT_SHA = "c3a03ac85f9fc8db8fa8225419aef8e3f57fe815";
+  public static final String GIT_DATE = "2025-09-26 11:36:41 EDT";
   public static final String GIT_BRANCH = "elevator-motor-fix";
-  public static final String BUILD_DATE = "2025-09-25 14:20:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1758824401740L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-09-26 11:37:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1758901070990L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
