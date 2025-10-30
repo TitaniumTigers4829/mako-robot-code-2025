97ab8c8b68882cf28f67fc534911a4173da8da35
Abigail-27 <ilovecats333meow@gmail.com>
2025-10-30 18:00:42 -0400

    W mentor

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "mako-robot-code-2025-3";
+  public static final String MAVEN_NAME = "mako-robot-code-2025-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 250;
-  public static final String GIT_SHA = "945b898b175c10bba935b2d3cb051739c45f29a6";
-  public static final String GIT_DATE = "2025-09-16 14:32:52 EDT";
+  public static final int GIT_REVISION = 251;
+  public static final String GIT_SHA = "dee796a2681949673f6dc459fabd2d387a6d8779";
+  public static final String GIT_DATE = "2025-10-27 13:57:27 EDT";
   public static final String GIT_BRANCH = "max-otb";
-  public static final String BUILD_DATE = "2025-10-24 18:58:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1761346738076L;
+  public static final String BUILD_DATE = "2025-10-30 17:55:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1761861309445L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
