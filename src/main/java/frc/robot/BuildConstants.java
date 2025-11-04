d8ef7a9166df46bd4564fa366b5c00d34a57c4b4
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2025-11-04 17:19:41 -0500

    whatever i did here

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "mako-robot-code-2025-3";
+  public static final String MAVEN_NAME = "mako-robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 260;
-  public static final String GIT_SHA = "2cbdf35e0443936dcd35a7466e95df9dffe2b6c0";
-  public static final String GIT_DATE = "2025-10-31 16:13:48 EDT";
-  public static final String GIT_BRANCH = "elevator-motor-fix";
-  public static final String BUILD_DATE = "2025-10-31 16:50:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1761943830817L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 276;
+  public static final String GIT_SHA = "bf90cb88d562680da03a4261fe3789372e97fdce";
+  public static final String GIT_DATE = "2025-11-04 00:38:34 EST";
+  public static final String GIT_BRANCH = "odometry-sim";
+  public static final String BUILD_DATE = "2025-11-04 10:54:50 EST";
+  public static final long BUILD_UNIX_TIME = 1762271690418L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
